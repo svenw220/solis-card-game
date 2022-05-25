75af6898023c7090cc3762d32c48d6931b6fd49c
Tyler <brightsolutions4@outlook.com>
2022-05-25 14:38:51 -0400

    Add vue and buefy basic structure with router and vuex

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <!-- <router-view /> -->
+    <b-field label="Name">
+      <b-input v-model="name" />
+    </b-field>
+    <p>hello world</p>
+  </div>
+</template>
+<script>
+/* ============
+ * Entry Point
+ * ============
+ *
+ * The entry point of the application.
+ */
+
+export default {
+  /**
+   * The name of the application.
+   */
+  name: 'VueBoilerplate',
+
+  /**
+   * Fires when the app has been mounted.
+   */
+  mounted() {
+    // If the user is authenticated,
+    // fetch the data from the API
+    // if (this.$store.state.auth.authenticated) {
+    //   this.$store.dispatch('account/find');
+    // }
+  },
+};
+</script>
