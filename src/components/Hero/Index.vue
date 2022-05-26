835f2d6b4cc9f7f1477581c7e2ac9b775efd3d4b
Tyler <brightsolutions4@outlook.com>
2022-05-26 02:14:54 -0400

    Refactor Hero section with flex structure

--- src/components/Hero/Index.vue
+++ src/components/Hero/Index.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="hero-container">
-    <div class="hero-title">
-      A grand card game of
-      <div>
-        <span class="highlight-text">battle, improvment</span>
-        and more
+    <div class="hero-content">
+      <div class="hero-title">
+        A grand card game of
+        <div>
+          <span class="highlight-text">battle, improvment</span>
+          and more
+        </div>
       </div>
-    </div>
-    <div class="hero-description">
-      DeFi Summer was just the beginning. Meet Inverse, part of the new wave of
-      decentralized banking & finance. From a capital-effcient money market.
-    </div>
-    <div class="hero-buttons-group">
-      <b-button type="is-primary" icon-right="delete">Get Access</b-button>
-      <b-button>Learn More</b-button>
+      <div class="hero-description">
+        DeFi Summer was just the beginning. Meet Inverse, part of the new wave
+        of decentralized banking & finance. From a capital-effcient money
+        market.
+      </div>
+      <div class="hero-buttons-group">
+        <b-button type="is-primary" icon-right="delete">Get Access</b-button>
+        <b-button>Learn More</b-button>
+      </div>
     </div>
     <img
       class="illustration"
@@ -47,34 +50,38 @@ export default {
 <style lang="scss" scoped>
 .hero-container {
   $root: '.hero';
-  width: 628px;
-  margin-top: 184px;
-  #{$root}-title {
-    font-size: 40px;
-    font-weight: 600;
-    margin-bottom: 1rem;
-  }
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 
-  .highlight-text {
-    font-weight: 700;
-  }
-
-  #{$root}-description {
-    font-size: 20px;
-    margin-bottom: 1rem;
-  }
-
-  #{$root}-buttons-group {
-    > button {
-      margin-right: 1rem;
+  #{$root}-content {
+    // width: 628px;
+    #{$root}-title {
+      font-size: 40px;
       font-weight: 600;
+      margin-bottom: 1rem;
+    }
+
+    .highlight-text {
+      font-weight: 700;
+    }
+
+    #{$root}-description {
+      font-size: 20px;
+      margin-bottom: 1rem;
+    }
+
+    #{$root}-buttons-group {
+      > button {
+        margin-right: 1rem;
+        font-weight: 600;
+      }
     }
   }
 
   .illustration {
-    position: absolute;
-    top: 2rem;
-    right: 10px;
+    position: relative;
+    top: -2rem;
   }
 }
 </style>
