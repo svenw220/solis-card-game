37cdb042673711ac881a3b7540cd7078a1737dc8
Tyler <brightsolutions4@outlook.com>
2022-05-27 01:23:28 -0400

    Add a single nft card

---
+++ src/components/NftLaunch/Index.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="nft-launch columns"></section>
+</template>
+
+<script>
+/* ============
+ * Home Index Page
+ * ============
+ *
+ * The home index page.
+ */
+
+export default {
+  /**
+   * The name of the page.
+   */
+  name: 'NftLaunch',
+
+  /**
+   * The components that the page can use.
+   */
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
