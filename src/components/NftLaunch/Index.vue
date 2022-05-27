a0c1cda17d3ed28fc4eb94f3b2d6c64b27e97151
Tyler <brightsolutions4@outlook.com>
2022-05-27 08:42:52 -0400

    Add basic NFT Launch component and needs to be add more elements

--- src/components/NftLaunch/Index.vue
+++ src/components/NftLaunch/Index.vue
@@ -1,8 +1,31 @@
 <template>
-  <section class="nft-launch columns"></section>
+  <section class="nft-launch columns">
+    <div class="column launch-content">
+      <div class="launch-prelead">We are launching our game assets</div>
+      <div class="launch-title has-text-weight-semibold">
+        Sapienoids NFTs Launch
+      </div>
+      <div class="launch-description mt-4">
+        Our trademark visuals, the headshot / profile pictures of RaidParty.
+        Lead your fighters into battle with your hero!
+      </div>
+      <b-button class="coming-soon">Comming Soon</b-button>
+    </div>
+    <div class="column nft-assets">
+      <div class="columns">
+        <div class="column group-1">
+          <NftCard />
+          <NftCard />
+        </div>
+        <div class="column group-2">2</div>
+        <div class="column group-3">3</div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import NftCard from '@/components/NftCard/Index.vue';
 /* ============
  * Home Index Page
  * ============
@@ -19,8 +42,32 @@ export default {
   /**
    * The components that the page can use.
    */
-  components: {},
+  components: {
+    NftCard,
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.nft-launch {
+  margin-top: 5rem;
+  align-items: center;
+}
+
+.launch-prelead {
+  color: #74737d;
+}
+
+.launch-title {
+  font-size: 2rem;
+  line-height: 52px;
+}
+
+.launch-content {
+  margin-right: 134px;
+}
+.coming-soon {
+  margin-top: 22px;
+  font-weight: 560;
+}
+</style>
--- src/views/Landing/Index.vue
+++ src/views/Landing/Index.vue
@@ -5,6 +5,7 @@
     <Lure />
     <How />
     <BuildingBlocks />
+    <NftLaunching />
     <Footer />
   </div>
 </template>
@@ -22,7 +23,7 @@ import Hero from '@/components/Hero/Index.vue';
 import Lure from '@/components/Lure/Index.vue';
 import How from '@/components/How/Index.vue';
 import BuildingBlocks from '@/components/BuildingBlocks/Index.vue';
-
+import NftLaunching from '@/components/NftLaunch/Index.vue';
 import Footer from '@/layouts/Footer/Index.vue';
 
 export default {
@@ -40,6 +41,7 @@ export default {
     Lure,
     How,
     BuildingBlocks,
+    NftLaunching,
     Footer,
   },
 };
