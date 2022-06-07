f98164d3802d8340c464b07e581efe797c33f466
Tyler <brightsolutions4@outlook.com>
2022-06-07 08:13:51 -0400

    Add draft avatar component

---
+++ src/components/Avatar/Index.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="avatar">
+    <b-image
+      :src="require('@assets/images/avatar.svg')"
+      alt="Player Avatar"
+    ></b-image>
+    <p class="player-name">You</p>
+    <p class="wallet-address">0x5478...26A031</p>
+    <b-image :src="require('@assets/images/action.svg')"></b-image>
+  </section>
+</template>
+
+<script>
+/* ============
+ * Home Index Page
+ * ============
+ *
+ * The home index page.
+ */
+
+export default {
+  /**
+   * The name of the page.
+   */
+  name: 'Avatar',
+  /**
+   * The components that the page can use.
+   */
+  components: {},
+  props: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
