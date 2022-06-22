2e37a27708750d758e51ef47a85bf40c746421ba
Tyler <brightsolutions4@outlook.com>
2022-06-21 22:44:16 -0400

    Add common constants

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,12 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/recommended',
-    '@vue/airbnb',
-  ],
+  extends: ['plugin:vue/recommended', '@vue/airbnb'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'import/no-cycle': 'off',
+    'linebreak-style': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
---
+++ src/const/index.js
@@ -0,0 +1,11 @@
+const myTurn = 'mine';
+const oppoTurn = 'oppo';
+const myArena = 'myArena';
+const oppoArena = 'OppoArena';
+
+export default {
+  myTurn,
+  oppoTurn,
+  myArena,
+  oppoArena,
+};
