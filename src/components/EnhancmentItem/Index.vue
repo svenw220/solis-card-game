ada3ec37501701cdca5e235abc56bd4ba14a4909
Tyler <brightsolutions4@outlook.com>
2022-06-29 22:01:07 -0400

    Add basic property component

---
+++ src/components/EnhancmentItem/Index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="enhancment-item">
+    <div class="item-label">HP</div>
+    <div class="item-content">
+      <div class="minus-button">-</div>
+      <div class="value">0</div>
+      <div class="plus-button">+</div>
+    </div>
+  </div>
+</template>
+
+<script>
+/* ============
+ * Home Index Page
+ * ============
+ *
+ * The home index page.
+ */
+
+export default {
+  /**
+   * The name of the page.
+   */
+  name: 'EnhancmentItem',
+  /**
+   * The components that the page can use.
+   */
+
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.enhancment-item {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  .item-content {
+    display: flex;
+    flex-direction: row;
+  }
+}
+</style>
