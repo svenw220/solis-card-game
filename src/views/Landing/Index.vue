eb3f0d067c74c4588adfd61a29b97dbd74ae488a
Tyler <brightsolutions4@outlook.com>
2022-07-02 22:47:26 -0400

    Remove unnecessary comments

--- src/views/Landing/Index.vue
+++ src/views/Landing/Index.vue
@@ -13,13 +13,6 @@
 </template>
 
 <script>
-/* ============
- * Home Index Page
- * ============
- *
- * The home index page.
- */
-
 import Navbar from '@/layouts/Navbar/Index.vue';
 import Hero from '@/components/Hero/Index.vue';
 import Lure from '@/components/Lure/Index.vue';
@@ -31,14 +24,7 @@ import Community from '@/components/Community/Index.vue';
 import Footer from '@/layouts/Footer/Index.vue';
 
 export default {
-  /**
-   * The name of the page.
-   */
   name: 'Landing',
-
-  /**
-   * The components that the page can use.
-   */
   components: {
     Navbar,
     Hero,
