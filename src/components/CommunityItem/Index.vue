2edcec6f5679f9b86b96c8817931da318aca3257
Tyler <brightsolutions4@outlook.com>
2022-07-04 10:30:18 -0400

    Add draft community item component

---
+++ src/components/CommunityItem/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="community-item"></div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.community-item {
+}
+</style>
