bb57bfeb2c1f6b9f5da7afbe6e6fa92ab22b96c5
sven <wietuschsven21@gmail.com>
2022-08-01 22:42:03 -0400

    Add basic battle result component

---
+++ src/components/BattleResult/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="battle-result">
+    <b-button type="is-primary">Back to matchmaking</b-button>
+    <b-button>Go to home</b-button>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ResultPopup',
+  components: {},
+  props: {
+    battleResult: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  computed: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.battle-result {
+}
+</style>
