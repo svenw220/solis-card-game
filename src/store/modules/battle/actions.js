d00e577605cd75ef374f25d400fd0d9fbed73ffa
sven <wietuschsven21@gmail.com>
2022-08-04 22:55:27 -0400

    Add simple battle vuex module

---
+++ src/store/modules/battle/actions.js
@@ -0,0 +1,16 @@
+/* ============
+ * Actions for the auth module
+ * ============
+ *
+ * The actions that are available on the
+ * auth module.
+ */
+// import { onValue } from 'firebase/database';
+// import db from '@/firebase';
+import * as types from './mutation-types';
+
+export default {
+  setCardInfo: ({ commit }, payload) => {
+    commit(types.SET_PLAYERS_CARD_INFO, payload);
+  },
+};
---
+++ src/store/modules/battle/state.js
@@ -0,0 +1,19 @@
+/* ============
+ * State of the game module
+ * ============
+ *
+ * The initial state of the game module.
+ */
+
+export default {
+  board: [
+    {
+      id: 'myInfo',
+      slots: [],
+    },
+    {
+      id: 'oppoInfo',
+      slots: [],
+    },
+  ],
+};
