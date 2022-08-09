1fac88085c20192dc0dcc7b7a839417b76d91a9a
sven <wietuschsven21@gmail.com>
2022-08-09 07:42:19 -0400

    Add basic structure of attack logic

--- src/utils/calc.js
+++ src/utils/calc.js
@@ -1,18 +1,38 @@
-const engine = (myCard, oppoCard) => {
+const strategyData = [
+  [[0, 0], [1, 2], [2, 1]],
+  [[0, 1], [1, 0], [2, 2]],
+];
+
+const engine = (myCard, oppoCard, firstTurn = false) => {
+  const attackedMyCard = {};
+  const attackedOppoCard = {};
+  const [myStrategy, oppoStrategy] = strategyData.flat(0);
+
   const {
-    ability: { myAttack, myDefense, myEvasion, myAccuracy },
-    type,
+    ability: { attack: myAttack, evasion: myEvasion, accuracy: myAccuracy, hp: myHp },
+    type: myCardType,
   } = myCard;
 
   const {
-    ability: { oppoAttack, oppoDefense, oppoEvasion, oppoAccuracy },
-    type,
+    ability: { attack: oppoAttack, evasion: oppoEvasion, accuracy: oppoAccuracy, hp: oppoHp },
+    type: oppoCardType,
   } = oppoCard;
 
-  myCard.hp -= (oppoAttack * (oppoAccuracy - myEvasion)) / 100;
-  oppoCard.hp -= (myAttack * (myAccuracy - oppoEvasion)) / 100;
+  console.log(myCardType, oppoCardType);
 
-  return { myCard, oppoCard };
+  for(const i of myStrategy) {
+    for (const j of oppoStrategy) {
+      
+    }
+  }
+  
+  if (firstTurn) {
+    attackedOppoCard.hp = oppoHp - (myAttack * (myAccuracy - oppoEvasion)) / 100;
+  } else {
+    attackedMyCard.hp = (oppoAttack * (oppoAccuracy - myEvasion)) / 100;
+  }  
+
+  return { attackedMyCard, attackedOppoCard };
 };
 
 export default {
